44fb6d1fb961d689f8cce927d13456212b0f54ba
曹稍 <caoshao@re-byods-146-50-221-205.wireless.uva.nl>
2025-11-10 14:34:14 +0100

    added "Alcohol data". added "wenyuan.R" including tidying process for comments, like, login, post.

---
+++ wenyuan.R
@@ -0,0 +1,10 @@
+# tidying: Comment.csv, Post.csv, Like.csv, Login.csv
+library(tidyverse)
+
+# data importing
+comment <- read_csv("~/Desktop/Alcohol_A/AlcoholData/Comment.csv")
+like <- read_csv("~/Desktop/Alcohol_A/AlcoholData/Like.csv")
+login <- read_csv("~/Desktop/Alcohol_A/AlcoholData/Login.csv")
+post <- read_csv("~/Desktop/Alcohol_A/AlcoholData/Post.csv")
+
+# 
